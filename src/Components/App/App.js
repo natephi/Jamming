c16cd783c69f8b89cc19c8c35f740f95c3918f29
Natephi <nate2world@gmail.com>
2021-03-26 07:48:30 +0000

    first commit

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,12 +3,23 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+  <div className="App">
+    // Add a SearchBar component
+    <div className="App-playlist">
+      // Add a SearchResults component
+    // Add a Playlist component
+    </div>
+  </div>
+</div>
+
+  
+
         <a
           className="App-link"
           href="https://reactjs.org"
